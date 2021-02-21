e3913e73d04ac24bfd6f6a2f3182320e1d391d8d
Tania Gutierrez <riobuenodevelops@gmail.com>
2021-02-20 21:55:54 -0400

    Initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Alert } from "rsuite"
 
 function App() {
+  const onClick = () => {
+    Alert.error("There's an error. Try Again", 10000);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button appearance="primary" onClick={onClick}>Registrarse</Button>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/styles/theme/index.css';
 import App from './App';
-import { i18n } from 'element-react';
-import locale from 'element-react/src/locale/lang/es';
 
-i18n.use(locale);
-
+import 'rsuite/lib/styles/index.less';
+import "./assets/styles/custom-theme.less";
 
 ReactDOM.render(
   <React.StrictMode>
