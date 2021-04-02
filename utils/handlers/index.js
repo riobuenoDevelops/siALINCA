9d987f005cc1d4e9f5dcb4ea0cd82db5ee765379
Tania Gutierrez <riobuenodevelops@gmail.com>
2021-04-02 01:36:30 -0400

    Removing debbugers

--- utils/handlers/index.js
+++ utils/handlers/index.js
@@ -2,33 +2,33 @@ const { config } = require("../../config/index");
 const boom = require("@hapi/boom");
 
 function withStack(error, stack) {
-	if (config.dev) {
-		logErrors(error);
-		return { ...error, stack };
-	}
-	return error;
+  if (config.dev) {
+    logErrors(error);
+    return { ...error, stack };
+  }
+  return error;
 }
 
 function wrapError(err, req, res) {
-	if (!err.isBoom) {
-		res.json(boom.badImplementation(err));
-		return false;
-	}
-	return true;
+  if (!err.isBoom) {
+    res.json(boom.badImplementation(err));
+    return false;
+  }
+  return true;
 }
 
 function logErrors(error) {
-	console.log(error);
+  console.log(error);
 }
 
 function errorHandler(err, req, res) {
-	if (wrapError(err, req, res)) {
-		const {
-			output: { statusCode, payload },
-		} = err;
-		res.status(statusCode);
-		res.json(withStack(payload, err.stack));
-	}
+  if (wrapError(err, req, res)) {
+    const {
+      output: { statusCode, payload },
+    } = err;
+    res.status(statusCode);
+    res.json(withStack({ ...payload, message: err.message }, err.stack));
+  }
 }
 
 module.exports = { errorHandler };
