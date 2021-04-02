37cc0f33dcc4817bca14bb1046ded6476a610b87
Tania Gutierrez <riobuenodevelops@gmail.com>
2021-04-02 12:27:12 -0400

    Adding the disabled field

--- utils/models/User.js
+++ utils/models/User.js
@@ -1,47 +1,47 @@
 const userSchema = {
-	$id: "user",
-	type: "object",
-	properties: {
-		_id: { $ref: "modelId#" },
-		names: { type: "string" },
-		lastNames: { type: "string" },
-		email: { type: "string" },
-		password: { type: "string" },
-		roleId: { $ref: "role#/properties/_id" },
-		config: {
-			type: "object",
-			properties: {
-				nivelInventario: { type: "integer" },
-			},
-		},
-		disabled: { type: "boolean" },
-	},
-	required: ["names", "lastNames", "email", "password", "roleId"],
-	additionalProperties: false,
+  $id: "user",
+  type: "object",
+  properties: {
+    _id: { $ref: "modelId#" },
+    names: { type: "string" },
+    lastNames: { type: "string" },
+    email: { type: "string" },
+    password: { type: "string" },
+    roleId: { $ref: "role#/properties/_id" },
+    config: {
+      type: "object",
+      properties: {
+        nivelInventario: { type: "integer" },
+      },
+    },
+    disabled: { type: "boolean" },
+  },
+  required: ["names", "lastNames", "email", "password"],
+  additionalProperties: true,
 };
 
 const updatedUserSchema = {
-	$id: "updateUser",
-	type: "object",
-	properties: {
-		_id: { $ref: "modelId#" },
-		names: { type: "string" },
-		lastNames: { type: "string" },
-		email: { type: "string" },
-		password: { type: "string" },
-		roleId: { $ref: "role#/properties/_id" },
-		config: {
-			type: "object",
-			properties: {
-				nivelInventario: { type: "integer" },
-			},
-		},
-		disabled: { type: "boolean" },
-	},
-	additionalProperties: false,
+  $id: "updateUser",
+  type: "object",
+  properties: {
+    _id: { $ref: "modelId#" },
+    names: { type: "string" },
+    lastNames: { type: "string" },
+    email: { type: "string" },
+    password: { type: "string" },
+    roleId: { $ref: "role#/properties/_id" },
+    config: {
+      type: "object",
+      properties: {
+        nivelInventario: { type: "integer" },
+      },
+    },
+    disabled: { type: "boolean" },
+  },
+  additionalProperties: false,
 };
 
 module.exports = {
-	userSchema,
-	updatedUserSchema,
+  userSchema,
+  updatedUserSchema,
 };
