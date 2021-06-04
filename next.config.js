f13e4a3de4c7c74427f89ff517520e9c56a9cc76
Tania Gutierrez <riobuenodevelops@gmail.com>
2021-06-04 19:42:13 -0400

    Adding dependencies

--- next.config.js
+++ next.config.js
@@ -1,25 +1,30 @@
 const withLess = require("@zeit/next-less");
 const withImages = require("next-images");
+
 module.exports = withImages(
-	withLess({
-		lessLoaderOptions: {
-			lessOptions: {
-				javascriptEnabled: true,
-				modifyVars: {
-					"@base-color": "#00B3EB",
-					"@warning-color": "#FF8727",
-					"@warning-light-color": "#FFB785",
-				},
-			},
-		},
-		async redirects() {
-			return [
-				{
-					source: "/",
-					destination: "/login",
-					permanent: true,
-				},
-			];
-		},
-	}),
+  withLess({
+    lessLoaderOptions: {
+      lessOptions: {
+        javascriptEnabled: true,
+        modifyVars: {
+          "@base-color": "#00B3EB",
+          "@warning-color": "#FF8727",
+          "@warning-light-color": "#FFB785",
+        },
+      },
+    },
+    env: {
+      guestUserEmail: "invitado@invitado.com",
+      guestUserPassword: "",
+    },
+    async redirects() {
+      return [
+        {
+          source: "/",
+          destination: "/login",
+          permanent: true,
+        },
+      ];
+    },
+  })
 );
