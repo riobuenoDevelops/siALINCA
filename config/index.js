72e8d63702e377e911d9cfae45b8bfa055c97a49
Tania Gutierrez <riobuenodevelops@gmail.com>
2021-06-04 19:38:47 -0400

    Parsing env variables and routes

--- config/index.js
+++ config/index.js
@@ -1,14 +1,15 @@
 require("dotenv").config({ path: __dirname + "/./../.env" });
 
 const config = {
-	dev: process.env.NODE_ENV !== "production",
-	url: process.env.URL || "http://localhost:3000",
-	dbHost: process.env.DB_HOST,
-	dbPort: process.env.DB_PORT,
-	dbName: process.env.DB_NAME,
-	dbUser: process.env.DB_USER,
-	dbPassword: process.env.DB_PASSWORD,
-	authJwtSecret: process.env.AUTH_JWT_SECRET,
+  dev: process.env.NODE_ENV !== "production",
+  url: process.env.URL || "http://localhost:3000",
+  dbHost: process.env.DB_HOST,
+  dbPort: process.env.DB_PORT,
+  dbName: process.env.DB_NAME,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  authJwtSecret: process.env.AUTH_JWT_SECRET,
+  apiUrl: `${this.url}/${process.env.API_URL}`,
 };
 
 module.exports = { config };
---
+++ config/routes.js
@@ -0,0 +1,14 @@
+const routes = {
+  login: "auth/login",
+  getUsers: "/users",
+  getStores: "/stores",
+  newUser: "/users",
+  updateUser: "/users",
+  newStore: "/stores",
+  getMeasures: "/measures",
+  sedes: "/sedes",
+  applicants: "/applicants",
+  items: "/items"
+};
+
+module.exports = routes;
