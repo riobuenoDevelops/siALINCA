0b10b16c03d39bfcccc6adf5773a0349ff13717b
Tania Gutierrez <riobuenodevelops@gmail.com>
2021-06-26 22:46:04 -0400

    Add router to NotesMenu

--- components/Header/NotesMenu.js
+++ components/Header/NotesMenu.js
@@ -2,17 +2,42 @@ import { Dropdown, Icon } from "rsuite";
 
 import "../../styles/custom-header.less";
 
-const NotesMenu = ({ history, location }) => {
+const NotesMenu = ({ router }) => {
+	const onSelect = (eventKey) => {
+		switch (eventKey) {
+			case "1":
+				router.push({
+					pathname: `${router.pathname}/new-delivery-note`,
+					query: { type: "CR" }
+				});
+				break;
+			case "2":
+				router.push({
+					pathname: `${router.pathname}/new-delivery-note`,
+					query: { type: "SR" }
+				});
+				break;
+		}
+	}
+
 	return (
 		<Dropdown
 			title="Nueva Orden"
 			icon={<Icon icon="plus" style={{ color: "white" }} />}
 			placement="bottomEnd"
 			className="menu-dropdown">
-			<Dropdown.Item className="dropdown-item">
+			<Dropdown.Item
+				className="dropdown-item"
+				eventKey="1"
+				onSelect={onSelect}
+			>
 				<Icon icon="level-down" /> Con Retorno
 			</Dropdown.Item>
-			<Dropdown.Item className="dropdown-item">
+			<Dropdown.Item
+				className="dropdown-item"
+				eventKey="2"
+				onSelect={onSelect}
+			>
 				<Icon icon="level-up" /> Sin Retorno
 			</Dropdown.Item>
 		</Dropdown>
