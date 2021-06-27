4a2c12db3cdaecca367cb92dcebaf2547d715e49
Tania Gutierrez <riobuenodevelops@gmail.com>
2021-06-26 22:53:05 -0400

    Add put method to route

--- pages/api/v1/stores/[id]/items.js
+++ pages/api/v1/stores/[id]/items.js
@@ -18,9 +18,24 @@ async function postMethodHandler(req, res) {
   }
 }
 
-async function getMethodHandler(req, res) {
+async function putMethodHandler(req, res) {
   try {
-    const storeItems = await StoreService.getStoreItems({ ids: req.body });
+    const {
+      query: { id },
+    } = req;
+
+    const storeId = await StoreService.updateStoreItems({ id, items: req.body });
+
+    res.status(200).json(storeId);
+  } catch (err) {
+    errorHandler(boom.internal(err), req, res);
+  }
+}
+
+async function getMethodHandler(req, res) {
+  const { query: { id } } = req;
+  try {
+    const storeItems = await StoreService.getStoreItems({ id });
     res.status(200).json(storeItems);
   } catch (err) {
     errorHandler(boom.internal(err), req, res);
@@ -37,10 +52,13 @@ export default authenticated(async function (req, res) {
     case "POST":
       scopeValidator(["update:store"], req, res, postMethodHandler);
       break;
+    case "PUT":
+      scopeValidator(["update:store"], req, res, putMethodHandler);
+      break;
     default:
       errorHandler(
         boom.methodNotAllowed(
-          "This endpoint only accepts GET, PUT and DELETE requests"
+          "This endpoint only accepts GET, PUT requests"
         ),
         req,
         res
