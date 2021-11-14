6a67fe597a2712a59e29f414d0db83c038df92cc
Tania Gutierrez <riobuenodevelops@gmail.com>
2021-11-14 18:51:33 -0400

    Added conditional props to render some components

--- components/tables/common/CustomDropdownMenu.js
+++ components/tables/common/CustomDropdownMenu.js
@@ -1,17 +1,34 @@
 import { Dropdown, Icon } from "rsuite";
 
-const CustomDropdownMenu = ({ onSelect, rowData, hasDeleted }) => (
+const CustomDropdownMenu = ({
+  isNotAdmin,
+  onSelect,
+  rowData,
+  hasDeleted,
+  hasPDF,
+  withoutEnable,
+  withoutEdit
+}) => (
   <Dropdown.Menu>
-    <Dropdown.Item eventKey={1} onSelect={onSelect}>
-      <Icon icon="edit" /> Editar
-    </Dropdown.Item>
-    {!hasDeleted && <Dropdown.Item divider />}
-    <Dropdown.Item eventKey={2} onSelect={onSelect}>
-      <Icon
-        icon={rowData.disabled ? "circle" : "circle-o"}
-      />
-      <span>{rowData.disabled ? "Habilitar" : "Deshabilitar"}</span>
-    </Dropdown.Item>
+    {!withoutEdit &&
+      <Dropdown.Item eventKey={1} onSelect={onSelect}>
+        <Icon icon="edit" /> Editar
+      </Dropdown.Item>
+    }
+    {hasPDF &&
+      <Dropdown.Item eventKey={4} onSelect={onSelect}>
+        <Icon icon="file" /> Generar PDF
+      </Dropdown.Item>
+    }
+    {!hasDeleted && !isNotAdmin && <Dropdown.Item divider />}
+    {!withoutEnable &&
+      <Dropdown.Item eventKey={2} onSelect={onSelect}>
+        <Icon
+          icon={rowData.disabled ? "circle" : "circle-o"}
+        />
+        <span>{rowData.disabled ? "Habilitar" : "Deshabilitar"}</span>
+      </Dropdown.Item>
+    }
     {hasDeleted &&
       <>
         <Dropdown.Item divider />
