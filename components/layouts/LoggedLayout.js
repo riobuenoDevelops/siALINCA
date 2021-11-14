92abe122c797a516cd77fc99252e4eca7b41bb6a
Tania Gutierrez <riobuenodevelops@gmail.com>
2021-11-14 18:56:40 -0400

    Formatted code and removed Ably Wrapper

--- components/layouts/LoggedLayout.js
+++ components/layouts/LoggedLayout.js
@@ -1,61 +1,65 @@
-import React, { useState } from "react";
-import { useRouter } from "next/router";
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import {
   Container,
   Header,
   Content,
   Sidebar,
   Icon,
-  IconButton,
-} from "rsuite";
+  IconButton
+} from 'rsuite';
 
-import CustomHeader from "../customComponents/CustomHeader";
-import SideNavbar from "./SideNavbar";
+import CustomHeader from '../customComponents/CustomHeader';
+import SideNavbar from './SideNavbar';
 
 //Styles
-import "../../styles/logged-layout.less";
-import AblyWrapper from '../common/AblyWrapper';
+import '../../styles/logged-layout.less';
+import { useCurrentUser } from '../../hooks';
 
 const LoggedLayout = ({
-  isLogged,
-  handleLogged,
-  children,
-  user,
-  handleUser
-}) => {
+                        children
+                      }) => {
   const router = useRouter();
+  const [isLogged, handleLogged] = useState(false);
   const [expanded, handleExpanded] = useState(true);
   const [userModalIsOpen, handleUserModalOpen] = useState(false);
   const [storeModalIsOpen, handleStoreModalOpen] = useState(false);
   const [sedeModalIsOpen, handleSedeModalOpen] = useState(false);
   const [applicantModalIsOpen, handleApplicantModalOpen] = useState(false);
   const [ablyClient, setAblyClient] = useState({});
+  const { user, isEmpty } = useCurrentUser();
 
   const onHandleExpanded = () => {
     handleExpanded(!expanded);
   };
 
+  useEffect(() => {
+    if (isEmpty) {
+      router.push('/login');
+    }
+  }, []);
+
   return (
     <Container className="logged-layout-container">
       <Sidebar
         className="logged-layout-sidebar"
-        style={{ display: isLogged ? "flex" : "none" }}
+        style={{ display: isLogged ? 'flex' : 'none' }}
         width={expanded ? 260 : 56}
         collapsible
       >
         <div
           style={{
-            width: "100%",
-            minHeight: "5em",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "flex-start",
-            marginBottom: "2em",
-            paddingLeft: ".68em",
+            width: '100%',
+            minHeight: '5em',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'flex-start',
+            marginBottom: '2em',
+            paddingLeft: '.68em'
           }}
         >
           <IconButton
-            icon={<Icon icon="bars" size="lg" style={{ color: "white" }} />}
+            icon={<Icon icon="bars" size="lg" style={{ color: 'white' }}/>}
             appearance="primary"
             onClick={onHandleExpanded}
           />
@@ -69,7 +73,7 @@ const LoggedLayout = ({
       <Container className="logged-layout-content-container">
         <Header
           className="logged-layout-header"
-          style={{ display: isLogged ? "initial" : "none" }}
+          style={{ display: isLogged ? 'initial' : 'none' }}
         >
           <CustomHeader
             handleUserModalOpen={handleUserModalOpen}
@@ -77,7 +81,6 @@ const LoggedLayout = ({
             handleSedeModalOpen={handleSedeModalOpen}
             handleApplicantModalOpen={handleApplicantModalOpen}
             user={user}
-            handleUser={handleUser}
             router={router}
             expanded={expanded}
             handleLogged={handleLogged}
@@ -87,25 +90,23 @@ const LoggedLayout = ({
         <Content
           className={
             isLogged
-              ? "logged-layout-content"
-              : "logged-layout-content-container"
+              ? 'logged-layout-content'
+              : 'logged-layout-content-container'
           }
         >
-          <AblyWrapper setParentClient={setAblyClient}>
-            {React.cloneElement(children, {
-              isLogged,
-              handleLogged,
-              handleUser,
-              handleUserModalOpen,
-              handleStoreModalOpen,
-              handleSedeModalOpen,
-              handleApplicantModalOpen,
-              userModalIsOpen,
-              storeModalIsOpen,
-              sedeModalIsOpen,
-              applicantModalIsOpen,
-            })}
-          </AblyWrapper>
+          {React.cloneElement(children, {
+            isLogged,
+            handleLogged,
+            user,
+            handleUserModalOpen,
+            handleStoreModalOpen,
+            handleSedeModalOpen,
+            handleApplicantModalOpen,
+            userModalIsOpen,
+            storeModalIsOpen,
+            sedeModalIsOpen,
+            applicantModalIsOpen
+          })}
         </Content>
       </Container>
     </Container>
