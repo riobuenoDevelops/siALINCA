14f20b3fda8f5ba6c179560f8ac90a93c628e3b0
Tania Gutierrez <riobuenodevelops@gmail.com>
2021-11-14 18:58:38 -0400

    Formatted code

--- components/customComponents/items/ElectroDeviceDetailRow.js
+++ components/customComponents/items/ElectroDeviceDetailRow.js
@@ -1,21 +1,21 @@
-import {FlexboxGrid} from "rsuite";
+import { FlexboxGrid } from 'rsuite';
 
 export default function ElectroDeviceDetailRow({ data }) {
   return (
     <>
-      <FlexboxGrid.Item style={{ marginRight: "1rem" }}>
+      <FlexboxGrid.Item style={{ marginRight: '1rem' }}>
         <span>Tipo de Dispositivo</span>
         <p>{data.deviceType}</p>
       </FlexboxGrid.Item>
-      <FlexboxGrid.Item style={{ marginRight: "1rem" }}>
+      <FlexboxGrid.Item style={{ marginRight: '1rem' }}>
         <span>Nro. de Serial</span>
         <p>{data.serial}</p>
       </FlexboxGrid.Item>
-      <FlexboxGrid.Item style={{ marginRight: "1rem" }}>
+      <FlexboxGrid.Item style={{ marginRight: '1rem' }}>
         <span>Marca</span>
         <p>{data.mark}</p>
       </FlexboxGrid.Item>
-      <FlexboxGrid.Item style={{ marginRight: "1rem" }}>
+      <FlexboxGrid.Item style={{ marginRight: '1rem' }}>
         <span>Modelo</span>
         <p>{data.model}</p>
       </FlexboxGrid.Item>
