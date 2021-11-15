6396c7ba1d838fbb3220c8f8e81021b1480d8bc6
Tania Gutierrez <riobuenodevelops@gmail.com>
2021-11-15 04:58:53 -0400

    Added CookieProvider and subscribed to notification channel

--- pages/_app.js
+++ pages/_app.js
@@ -1,38 +1,37 @@
-import Router from "next/router";
 import Head from "next/head";
-import { useState, useEffect } from "react";
+import electron from "electron";
+import { useEffect } from "react";
 import { I18nextProvider } from "react-i18next";
-import i18n from "../utils/i18n/index";
 import { SWRConfig } from "swr";
+import { CookiesProvider } from "react-cookie";
+import { Notification } from "rsuite";
+
+import i18n from "../utils/i18n/index";
 
 import LoggedLayout from "../components/layouts/LoggedLayout";
-import LoadingScreen from "../components/layouts/LoadingScreen";
 
 import AxiosService from "../services/Axios";
 
 import "rsuite/lib/styles/index.less";
 import "../styles/custom-theme.less";
 
-export default function MyApp({ Component, pageProps }) {
-  const [isLogged, handleLogged] = useState(false);
-  const [user, handleUser] = useState({});
-  const [loading, setLoading] = useState(false);
+const ipcRenderer = electron.ipcRenderer || false;
+
+export default function MyApp({ Component, ...pageProps }) {
 
   useEffect(() => {
-    const start = () => {
-      setLoading(true);
-    };
-    const end = () => {
-      setLoading(false);
-    };
-    Router.events.on("routeChangeStart", start);
-    Router.events.on("routeChangeComplete", end);
-    Router.events.on("routeChangeError", end);
+    ipcRenderer.on("notify-critical-item", (event, data) => {
+      Notification.warning({
+        title: "Nivel de Inventario Crítico",
+        description: `El item "${data}" ha superado el nivel mínimo de inventario`,
+        placement: "bottomEnd",
+        duration: "5000"
+      });
+    });
+
     return () => {
-      Router.events.off("routeChangeStart", start);
-      Router.events.off("routeChangeComplete", end);
-      Router.events.off("routeChangeError", end);
-    };
+      ipcRenderer.removeAllListeners("notify-critical-item");
+    }
   }, []);
 
   return (
@@ -41,37 +40,34 @@ export default function MyApp({ Component, pageProps }) {
         <title>Sistema de Inventario ALINCA</title>
       </Head>
       <I18nextProvider i18n={i18n}>
-          <SWRConfig
-            value={{
-              refreshInterval: 10000,
-              fetcher: (url, token, query) => {
-                if(!url) {
-                  return false;
-                }
-                return query
-                  ? AxiosService.instance.get(url, {
-                    headers: {
-                      Authorization: token
-                    },
-                    params: query
-                  }).then(res => res.data)
-                  : AxiosService.instance.get(url, {
-                    headers: {
-                      Authorization: token
-                    },
-                  }).then(res => res.data)
+        <SWRConfig
+          value={{
+            refreshInterval: 10000,
+            fetcher: (url, token, query) => {
+              if (!url) {
+                return false;
               }
-            }}
-          >
-            <LoggedLayout
-              isLogged={isLogged}
-              handleLogged={handleLogged}
-              user={user}
-              handleUser={handleUser}
-            >
-              {loading ? <LoadingScreen /> : <Component {...pageProps} />}
+              return query
+                ? AxiosService.instance.get(url, {
+                  headers: {
+                    Authorization: token
+                  },
+                  params: query
+                }).then(res => res.data)
+                : AxiosService.instance.get(url, {
+                  headers: {
+                    Authorization: token
+                  },
+                }).then(res => res.data)
+            }
+          }}
+        >
+          <CookiesProvider>
+            <LoggedLayout>
+              <Component {...pageProps} />
             </LoggedLayout>
-          </SWRConfig>
+          </CookiesProvider>
+        </SWRConfig>
       </I18nextProvider>
     </>
   );
