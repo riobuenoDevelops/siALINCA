80aada33b890b6cdf9b277995f17073c29239ab3
Tania Gutierrez <riobuenodevelops@gmail.com>
2021-11-15 04:57:43 -0400

    Added withoutDelete conditional prop and formatted code

--- components/customComponents/BasicActionsButtonGroup.js
+++ components/customComponents/BasicActionsButtonGroup.js
@@ -1,8 +1,8 @@
-import { Icon, IconButton, Tooltip, Whisper, Notification } from "rsuite";
+import { Icon, IconButton, Tooltip, Whisper, Notification } from 'rsuite';
 
-import AxiosService from "../../services/Axios";
+import AxiosService from '../../services/Axios';
 
-export default function BasicActionsButtonGroup({disabled, onEdit, onDelete, route, token, itemMutate}) {
+export default function BasicActionsButtonGroup({ disabled, onEdit, onDelete, route, token, itemMutate, withoutDelete }) {
 
   const onEnableDisable = async () => {
     try {
@@ -12,27 +12,32 @@ export default function BasicActionsButtonGroup({disabled, onEdit, onDelete, rou
         headers: {
           Authorization: token
         }
-      })
+      });
       await itemMutate();
-    }catch (err) {
+    } catch (err) {
       Notification.error({
-        title: "Error",
+        title: 'Error',
         description: err?.response?.data?.message,
         duration: 9000,
-        placement: "bottomStart"
-      })
+        placement: 'bottomStart'
+      });
     }
-  }
+  };
 
   return <>
     <Whisper placement="bottom" trigger="hover" speaker={<Tooltip>Editar</Tooltip>}>
-      <IconButton size="md" appearence="primary" circle style={{marginRight: "0.5rem"}} className="bg-default" icon={<Icon icon="edit" />} onClick={onEdit} />
+      <IconButton size="md" appearence="primary" circle style={{ marginRight: '0.5rem' }} className="bg-default"
+                  icon={<Icon icon="edit"/>} onClick={onEdit}/>
     </Whisper>
-    <Whisper placement="bottom" trigger="hover" speaker={<Tooltip>{disabled ? "Habilitar" : "Deshabilitar"}</Tooltip>}>
-      <IconButton size="md" appearence="primary" circle style={{marginRight: "0.5rem"}} className="bg-default" icon={<Icon icon={disabled ? "circle" : "circle-o"} />} onClick={onEnableDisable} />
+    <Whisper placement="bottom" trigger="hover" speaker={<Tooltip>{disabled ? 'Habilitar' : 'Deshabilitar'}</Tooltip>}>
+      <IconButton size="md" appearence="primary" circle className="bg-default"
+                  icon={<Icon icon={disabled ? 'circle' : 'circle-o'}/>} onClick={onEnableDisable}/>
     </Whisper>
-    <Whisper placement="bottom" trigger="hover" speaker={<Tooltip>Eliminar</Tooltip>}>
-      <IconButton size="md" appearence="primary" circle className="bg-default" icon={<Icon icon="trash" />} onClick={onDelete} />
-    </Whisper>
-  </>
+    {withoutDelete &&
+      <Whisper placement="bottom" trigger="hover" speaker={<Tooltip>Eliminar</Tooltip>}>
+        <IconButton size="md" appearence="primary" circle style={{ marginLeft: '0.5rem' }} className="bg-default" icon={<Icon icon="trash"/>}
+                    onClick={onDelete}/>
+      </Whisper>
+    }
+  </>;
 }
